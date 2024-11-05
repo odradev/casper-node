b3ec7ef0f60fd5be632c35c87231cdfaaaceee54
Karan Dhareshwar <karan@casperlabs.io>
2024-11-05 10:27:33 -0600

    Amend faucet costs

--- execution_engine_testing/tests/src/test/explorer/faucet.rs
+++ execution_engine_testing/tests/src/test/explorer/faucet.rs
@@ -663,7 +663,7 @@ fn faucet_costs() {
     // This test will fail if execution costs vary.  The expected costs should not be updated
     // without understanding why the cost has changed.  If the costs do change, it should be
     // reflected in the "Costs by Entry Point" section of the faucet crate's README.md.
-    const EXPECTED_FAUCET_INSTALL_COST: u64 = 141_721_606_176;
+    const EXPECTED_FAUCET_INSTALL_COST: u64 = 141_969_710_490;
 
     const EXPECTED_FAUCET_SET_VARIABLES_COST: u64 = 143_865_670;
     const EXPECTED_FAUCET_CALL_BY_INSTALLER_COST: u64 = 2_705_894_903;
