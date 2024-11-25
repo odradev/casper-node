8482bee1b7145de731496a47731789dedd6b86be
Michał Papierski <michal@papierski.net>
2024-11-25 15:13:28 +0000

    Account for stack height limiter
    Updates multipliers based on the output of a benchmark that also
    includes modified wasm with gas injector and stack limiter. This
    benchmark output should be as close as possible to measure real
    overhead of running modified Wasm.

--- execution_engine_testing/tests/src/test/explorer/faucet.rs
+++ execution_engine_testing/tests/src/test/explorer/faucet.rs
@@ -663,13 +663,13 @@ fn faucet_costs() {
     // This test will fail if execution costs vary.  The expected costs should not be updated
     // without understanding why the cost has changed.  If the costs do change, it should be
     // reflected in the "Costs by Entry Point" section of the faucet crate's README.md.
-    const EXPECTED_FAUCET_INSTALL_COST: u64 = 141_835_235_435;
+    const EXPECTED_FAUCET_INSTALL_COST: u64 = 141_843_520_894;
 
-    const EXPECTED_FAUCET_SET_VARIABLES_COST: u64 = 70_639_675;
+    const EXPECTED_FAUCET_SET_VARIABLES_COST: u64 = 70_775_980;
 
-    const EXPECTED_FAUCET_CALL_BY_INSTALLER_COST: u64 = 2_644_931_333;
+    const EXPECTED_FAUCET_CALL_BY_INSTALLER_COST: u64 = 2_645_047_453;
 
-    const EXPECTED_FAUCET_CALL_BY_USER_COST: u64 = 2_545_472_446;
+    const EXPECTED_FAUCET_CALL_BY_USER_COST: u64 = 2_545_669_471;
 
     let installer_account = AccountHash::new([1u8; 32]);
     let user_account: AccountHash = AccountHash::new([2u8; 32]);
